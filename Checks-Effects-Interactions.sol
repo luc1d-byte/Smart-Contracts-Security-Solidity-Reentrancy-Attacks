79b56144423af9fc633a9ad46041e8d3718e4552
luc1d-byte <111508361+luc1d-byte@users.noreply.github.com>
2022-08-19 18:59:42 -0300

    Update Checks-Effects-Interactions.sol

--- Checks-Effects-Interactions.sol
+++ Checks-Effects-Interactions.sol
@@ -5,17 +5,17 @@ contract Checks_Effects_Interactions {
     function withdraw(uint withdrawAmount) public returns (uint){
 
 		
-  		//Checks = verifications
+  	//Checks = verifications
         require(withdrawAmount <= balances[msg.sender]);
 	   		
-  		//Effects = actions that will be executed, 
-		//in this case, subtract the value of the wallet/contract
-  		balances[msg.sender] -= withdrawAmount;
+  	//Effects = actions that will be executed, 
+	//in this case, subtract the value of the wallet/contract
+  	balances[msg.sender] -= withdrawAmount;
   			
   
-  		//Interactions = withdraw the value of the wallet
-  		msg.sender.call.value(withdrawAmount)("");
+  	//Interactions = withdraw the value of the wallet
+  	msg.sender.call.value(withdrawAmount)("");
 
-		return balances[msg.sender];
+	return balances[msg.sender];
     }
 }
